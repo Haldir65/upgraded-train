2c8bd53afdc3b4669d8c21923c3f2ece18336b88
Haldir65 <mjw090608@gmail.com>
2024-01-31 18:02:35 +0800

    [build] trigger build action

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -60,9 +60,11 @@ esac
 # Run `cmake` to configure the project.
 cmake \
   -G Ninja \
-  -DCMAKE_BUILD_TYPE=MinSizeRel \
+  -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX="/" \
-  -DLLVM_ENABLE_PROJECTS="clang;lld" \
+  -DLLVM_ENABLE_PROJECTS="clang;lld;libc;libclc;lldb" \
+  -DLLVM_ENABLE_RUNTIMES=all  \
+  -DLLVM_ENABLE_RTTI=ON \
   -DLLVM_ENABLE_TERMINFO=OFF \
   -DLLVM_ENABLE_ZLIB=OFF \
   -DLLVM_INCLUDE_DOCS=OFF \
@@ -72,14 +74,14 @@ cmake \
   -DLLVM_INCLUDE_TOOLS=ON \
   -DLLVM_INCLUDE_UTILS=OFF \
   -DLLVM_OPTIMIZED_TABLEGEN=ON \
-  -DLLVM_TARGETS_TO_BUILD="X86;AArch64;RISCV;WebAssembly" \
+  -DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64;Mips" \
   "${CROSS_COMPILE}" \
   "${CMAKE_ARGUMENTS}" \
   ../llvm
 
 # Showtime!
-cmake --build . --config MinSizeRel
-DESTDIR=destdir cmake --install . --strip --config MinSizeRel
+cmake --build . --config Release
+DESTDIR=destdir cmake --install . --strip --config Release
 
 # move usr/bin/* to bin/ or llvm-config will be broken
 if [ ! -d destdir/bin ];then
