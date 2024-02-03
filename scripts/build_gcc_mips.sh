d43eba038cd072cbbfbdc74ca4b57c251db88cdb
harris <mjw090608@gmail.com>
2024-02-03 22:50:33 +0800

    [build] gcc 13 build

--- scripts/build_gcc_mips.sh
+++ scripts/build_gcc_mips.sh
@@ -2,16 +2,30 @@
 
 function main(){
     local current_dir=`pwd`
+
+    export PREFIX="${current_dir}/gcc-13-build"
+    export TARGET=mipsel-unknown-linux-gnu
+    export WDIR=gcc13build
     sudo apt install -y build-essential
     echo "[step] : download gcc source"
 
-    # git clone git://gcc.gnu.org/git/gcc.git gcc13source
 
-    mkdir -p gcc13build
+    tar xvf assets/gcc-13.2.0.tar.gz -C build
+
+    ## todo 
+    # https://wiki.osdev.org/GCC_Cross-Compiler#Preparation
+    ##https://www.linux-mips.org/wiki/Toolchains#Roll-your-own
+
+
+    # export PATH="$PREFIX/bin:$PATH"
+
+
+    # git clone git://gcc.gnu.org/git/gcc.git gcc13source
+    mkdir -p ${WDIR}
     mkdir -p ${current_dir}/gcc-13-build
     cd gcc13source
     # git checkout releases/gcc-13
-    echo "[step] : checkout releases/gcc-13 , show gcc source"
+    # echo "[step] : checkout releases/gcc-13 , show gcc source"
     ls -alSh
     ./contrib/download_prerequisites
     cd ../gcc13build
