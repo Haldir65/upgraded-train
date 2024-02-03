a3cf126bb604e0c33fecead744190f76d86d76a5
Haldir65 <mjw090608@gmail.com>
2024-02-03 18:41:06 +0800

    [build] gcc 13 build from source , modify scripts

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -6,7 +6,7 @@ function main(){
     echo "[step] : download gcc source"
     git clone git://gcc.gnu.org/git/gcc.git gcc13source
     mkdir -p gcc13build
-    mkdir -p {current_dir}/gcc-13-build
+    mkdir -p ${current_dir}/gcc-13-build
     cd gcc13source
     git checkout releases/gcc-13
     echo "[step] : checkout releases/gcc-13 , show gcc source"
@@ -22,7 +22,7 @@ function main(){
     make -j 4
     echo "now install begin"
     make install
-    ls -al ../gcc
+    # ls -al ../gcc-13-build
 
 }
 
