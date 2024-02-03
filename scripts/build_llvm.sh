1f43f3650bb1cfb0cd128662f4d200896834ca95
Haldir65 <mjw090608@gmail.com>
2024-02-03 18:48:46 +0800

    [build] llvm build tools , then build runtime

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -40,10 +40,10 @@ git reset --hard origin/"release/$LLVM_VERSION"
 
 # Create a directory to build the project.
 mkdir -p build
-cd build
+# cd build
 
 # Create a directory to receive the complete installation.
-mkdir -p install
+mkdir -p build/install
 
 # Adjust compilation based on the OS.
 CMAKE_ARGUMENTS=""
@@ -75,18 +75,11 @@ pwd
 # Run `cmake` to configure the project.
 cmake \
   -G Ninja \
-  -DCMAKE_CXX_COMPILER=clang++  \
-  -DCMAKE_C_COMPILER=clang  \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_ASSERTIONS=ON  \
   -DLLVM_OPTIMIZED_TABLEGEN=ON  \
-  -DLLVM_ENABLE_LLD=ON  \
-  -DLLVM_ENABLE_LIBCXX=ON  \
   -DCMAKE_INSTALL_PREFIX="/" \
-  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;libc;libclc;lldb" \
-  -DLLVM_ENABLE_RUNTIMES=all  \
-  -DLLVM_ENABLE_RTTI=ON \
-  -DLLVM_ENABLE_TERMINFO=OFF \
+  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" \
   -DLLVM_ENABLE_ZLIB=OFF \
   -DLLVM_INCLUDE_DOCS=OFF \
   -DLLVM_INCLUDE_EXAMPLES=OFF \
@@ -94,11 +87,37 @@ cmake \
   -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_INCLUDE_TOOLS=ON \
   -DLLVM_INCLUDE_UTILS=OFF \
-  -DLLVM_OPTIMIZED_TABLEGEN=ON \
   -DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64;Mips" \
   "${CROSS_COMPILE}" \
   "${CMAKE_ARGUMENTS}" \
-  ../llvm
+  -B build/llvm -S llvm
+
+ninja -C build/llvm
+
+
+cmake \
+  -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER="build/llvm/clang++"  \
+  -DCMAKE_C_COMPILER="build/llvm/clang"  \
+  -DLLVM_ENABLE_ASSERTIONS=ON  \
+  -DLLVM_OPTIMIZED_TABLEGEN=ON  \
+  -DCMAKE_INSTALL_PREFIX="/" \
+  -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
+  -DLLVM_ENABLE_ZLIB=OFF \
+  -DLLVM_INCLUDE_DOCS=OFF \
+  -DLLVM_INCLUDE_EXAMPLES=OFF \
+  -DLLVM_INCLUDE_GO_TESTS=OFF \
+  -DLLVM_INCLUDE_TESTS=OFF \
+  -DLLVM_INCLUDE_TOOLS=ON \
+  -DLLVM_INCLUDE_UTILS=OFF \
+  -DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64;Mips" \
+  "${CROSS_COMPILE}" \
+  "${CMAKE_ARGUMENTS}" \
+  -B build/runtimes -S runtimes
+
+ninja -C build/runtimes
+
 
 
 # cmake -G Ninja -S ../llvm  \
@@ -122,10 +141,12 @@ cmake \
 # ninja install-runtimes 
 # echo "num of processor is ${nproc}"
 
-clang++ --version
+# clang++ --versions
+
+
 # Showtime!
-cmake --build . --config Release -j2
-DESTDIR=destdir cmake --install . --strip --config Release
+# cmake --build . --config Release -j2
+DESTDIR=destdir cmake --install build --strip --config Release
 
 # move usr/bin/* to bin/ or llvm-config will be broken
 if [ ! -d destdir/bin ];then
