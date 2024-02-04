11c02c116877e12e434841ae40fafb00389c8956
harris <mjw090608@gmail.com>
2024-02-04 21:31:32 +0800

    [gcc] ditch arm cross build, use checkout action for gcc source

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -14,12 +14,12 @@ function main(){
     sudo apt install -y build-essential
     echo "[step] : download gcc source"
 
-    git clone git://gcc.gnu.org/git/gcc.git gcc13source
+    # git clone git://gcc.gnu.org/git/gcc.git gcc13source
 
     mkdir -p gcc13build
     mkdir -p ${current_dir}/gcc-13-build
     cd gcc13source
-    git checkout releases/gcc-13
+    # git checkout releases/gcc-13
     echo "[step] : checkout releases/gcc-13 , show gcc source"
     ls -alSh
     ./contrib/download_prerequisites
