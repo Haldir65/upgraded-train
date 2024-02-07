ec88a603b1a27c811fe299489fd9f02db677c887
harris <mjw090608@gmail.com>
2024-02-07 21:19:48 +0800

    [zsh] cross build and examine using docker image in different arch

--- scripts/build_zsh.sh
+++ scripts/build_zsh.sh
@@ -2,36 +2,66 @@
 
 ARCH=$1
 
-wget https://musl.cc/${ARCH}-cross.tgz
-tar zxvf ${ARCH}-cross.tgz
+CURRENT_DIR=`pwd`
 
-ROOT_DIR=`pwd`
+##https://github.com/romkatv/zsh-bin/issues/2#issuecomment-605519737
+# wget https://musl.cc/$ARCH-cross.tgz
+# wget https://github.com/userdocs/qbt-musl-cross-make/releases/download/2406/$ARCH.tar.xz
+# tar zxvf $$ARC.tar.xz
+
+ROOT_DIR=$CURRENT_DIR
+
+
+export SYSROOT="$ROOT_DIR"
+export CC=$ROOT_DIR/$ARCH/bin/$ARCH-gcc
+export CXX=$ROOT_DIR/$ARCH/bin/$ARCH-g++
+
 
 export CC_PREFIX="$ARCH"
-export SYSROOT="$CC_PREFIX-cross"
 export LIB_CACHE=$ROOT_DIR/$CC_PREFIX
-export PATH="$SYSROOT/bin:$PATH"
+# export PATH="$SYSROOT/bin:$PATH"
 export CFLAGS_EXTRA="-I$LIB_CACHE/include"
-export LDFLAGS_EXTRA="-static -L$LIB_CACHE/lib64 -L$LIB_CACHE/lib"
-export CC=$SYSROOT/bin/$ARCH-gcc
+export LDFLAGS_EXTRA="-static  -L$LIB_CACHE/lib64 -L$LIB_CACHE/lib"
+
+
 
 $CC --version
 
-git clone --depth 1 https://github.com/mirror/ncurses
+if [ ! -d ncurses ];then
+    git clone --depth 1 https://github.com/mirror/ncurses
+fi
+
+
 pushd ncurses
-
 echo "show gcc info"
-./configure --prefix="$LIB_CACHE" --host=$CC_PREFIX --with-build-cc=$CC --without-shared --disable-stripping --enable-widec --without-debug
-make
+$CC --version
+mkdir -p $CURRENT_DIR/ncurses_install
+CXXFLAGS="-fPIC" CFLAGS="-fPIC" \
+./configure --prefix="$CURRENT_DIR/ncurses_install" --host=$CC_PREFIX --with-build-cc=gcc --without-shared --disable-stripping --enable-widec --without-debug
+make -j ${nproc}
 make install
 popd
 
-wget https://downloads.sourceforge.net/project/zsh/zsh/5.8/zsh-5.8.tar.xz
-tar Jxvf zsh-5.8.tar.xz
+
+if [ ! -d zsh-5.8 ];then
+    if [ ! -f zsh-5.8.tar.xz ];then
+        wget https://downloads.sourceforge.net/project/zsh/zsh/5.8/zsh-5.8.tar.xz
+    fi
+    tar Jxvf zsh-5.8.tar.xz
+fi
 pushd zsh-5.8
-./configure --prefix="$LIB_CACHE" --host=$CC_PREFIX --enable-cflags="$CFLAGS_EXTRA" --enable-ldflags="$LDFLAGS_EXTRA"
-make
+mkdir -p $CURRENT_DIR/zsh_install
+CPPFLAGS="-I$CURRENT_DIR/ncurses_install/include" \
+./configure --prefix="$CURRENT_DIR/zsh_install" --host=$CC_PREFIX --enable-cflags="-I$CURRENT_DIR/ncurses_install/include $CFLAGS_EXTRA" --enable-ldflags="-L$CURRENT_DIR/ncurses_install/lib $LDFLAGS_EXTRA "
+make -j ${nproc}
 make install
 popd
 
-file $LIB_CACHE/bin/zsh # /root/aarch64-linux-musl/bin/zsh: ELF 64-bit LSB executable, ARM aarch64, version 1 (SYSV), statically linked, with debug_info, not stripped
+echo "you can find zsh staic binary ins \n ${CURRENT_DIR}/zsh_install/bin/zsh"
+
+
+file ${CURRENT_DIR}/zsh_install/bin/zsh
+
+
+mv ${CURRENT_DIR}/zsh_install ${ARCH}_dist
+
