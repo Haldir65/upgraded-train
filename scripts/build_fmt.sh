96bf3663b1e104595de82448a507ce61b7404de7
Haldir65 <mjw090608@gmail.com>
2024-03-05 11:36:54 +0800

    update on gh

--- scripts/build_fmt.sh
+++ scripts/build_fmt.sh
@@ -287,7 +287,7 @@ function _build_simple_cpp_program(){
     fi
     # $CC --version
     # $CXX --version
-    ls -al ${DIRECTORY}/lib/x86_64-unknown-linux-gnu
+    # ls -al ${DIRECTORY}/lib/x86_64-unknown-linux-gnu
     ${CXX} test/clangTest.cpp -std=c++20 -lpthread -stdlib=libstdc++ -v -Wall -o clangTest
     ./clangTest
 }
