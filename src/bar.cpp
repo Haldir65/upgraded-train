985be5bbca94dae10ff16f51995f17555d45b81d
Haldir65 <mjw090608@gmail.com>
2024-03-06 11:46:00 +0800

    [vcpkg] vcpkg feature

--- src/bar.cpp
+++ src/bar.cpp
@@ -59,9 +59,46 @@ void bar::performing_curl_test()
     if (curl)
     {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
+        curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        std::string_view version{curl_version()};
+        curl_version_info_data *vinfo = curl_version_info(CURLVERSION_NOW);
+        std::cout << "curl_version_info " << std::endl;
+
+        auto http2Version = vinfo->nghttp2_version;
+        if (http2Version)
+        {
+            fmt::print(fmt::fg(fmt::color::gold), "nghttp2_version = {0} \n", std::string(http2Version));
+            fmt::print(fmt::fg(fmt::color::gold), "nghttp2_ver_num = {0} \n", std::to_string(vinfo->nghttp2_ver_num));
+        }
+        else
+        {
+            std::cout << "nghttp2_version not found " << std::endl;
+            // return;
+        }
+
+        std::cout << "httpVersion 1 " << std::endl;
+
+        const char *httpVersion;
+        if (vinfo->version_num >= 0x072000)
+        {
+            httpVersion = "HTTP/2";
+        }
+        else if (vinfo->version_num >= 0x071800)
+        {
+            httpVersion = "HTTP/1.1";
+        }
+        else
+        {
+            httpVersion = "HTTP/1.0";
+        }
+        std::cout << "httpVersion 2 " << std::endl;
+
+        std::cout << "base HTTP Protocol Version: " << httpVersion << std::endl;
+
+        fmt::print(fmt::fg(fmt::color::light_green), "curl_version = {0} \n", std::string{version} + "\n");
 
         res = curl_easy_perform(curl);
 
@@ -73,6 +110,12 @@ void bar::performing_curl_test()
         {
             // 获取当前连接使用的HTTP协议版本
             curl_easy_getinfo(curl, CURLINFO_HTTP_VERSION, &http_version);
+
+            // 解析返回的JSON数据
+            nlohmann::json json_data = nlohmann::json::parse(response);
+            // 输出解析后的JSON数据
+            fmt::print(fmt::fg(fmt::color::red), "json_data {0}\n", json_data.dump(4));
+
             if (http_version == CURL_HTTP_VERSION_2_0)
             {
                 std::cout << "The connection is using HTTP/2 protocol." << std::endl;
@@ -81,11 +124,6 @@ void bar::performing_curl_test()
             {
                 std::cout << "The connection is not using HTTP/2 protocol." << std::endl;
             }
-
-            // 解析返回的JSON数据
-            nlohmann::json json_data = nlohmann::json::parse(response);
-            // 输出解析后的JSON数据
-            fmt::print(fmt::fg(fmt::color::light_green), "json_data {0}\n", json_data.dump(4));
         }
 
         curl_easy_cleanup(curl);
