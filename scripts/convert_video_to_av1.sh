17118d39c9184e837e00da1d45b39ee83fb6af76
Haldir65 <mjw090608@gmail.com>
2024-04-09 11:12:21 +0800

    [test] yt-dlp in action 4

--- scripts/convert_video_to_av1.sh
+++ scripts/convert_video_to_av1.sh
@@ -6,12 +6,22 @@ set -o errtrace
 
 function convert_all_video_to_av1_format(){
     mkdir -p dist
+    
     _green " creating directory now"
-    # 遍历当前目录下的所有视频文件
-    for file in *.mp4 *.mkv *.webm *.avi *.mov *.flv *.wmv *.ogg *.ogv *.qt *.m4v *.mpg *.mpeg *.3gp *.asf *.rm *.swf
-    do
+    # 开启nullglob选项以防止在没有匹配文件的情况下进入循环
+    shopt -s nullglob
+    # 创建一个空数组来存储文件名
+    files=()
+    # 使用for循环列举出以.webm或者.mp4为后缀的文件，并将它们添加到数组中
+    for file in *.webm *.mp4 *.mkv *.m4v; do
+    files+=("$file")
+    done
+
+    # 遍历数组，并输出每一个文件名
+    for file in "${files[@]}"; do
+        _green "found video file: $file"
         _green " processing file $file\n"
-        du -sh $file
+        du -sh "$file"
         # 使用FFmpeg将视频文件转码为AV1格式
         ffmpeg -i "$file" -c:v libaom-av1 -crf 30 -c:s copy -c:a copy "${file%.*}.av1.mp4"
         rm -rf $file
